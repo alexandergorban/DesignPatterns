65e8b93cf0e814aeebd5ea32f622df0215237285
Alexander Gorban <gorban.alexander.y@gmail.com>
2020-03-31 15:41:05 +0300

    Add project files.

---
+++ StateDesignPattern/UI/StatusPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace StateDesignPattern.UI
+{
+    /// <summary>
+    /// Interaction logic for StatusPage.xaml
+    /// </summary>
+    public partial class StatusPage : Page
+    {
+        public StatusPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
