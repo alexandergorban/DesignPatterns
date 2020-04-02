5006ef8fd354e4efb752741132011e1162c8bfe8
Alexander Gorban <gorban.alexander.y@gmail.com>
2020-04-02 12:33:44 +0300

    Strategy - Extend SwedenSalesTaxStrategy

--- StrategyPattern/Business/Strategies/SalesTax/SwedenSalesTaxStrategy.cs
+++ StrategyPattern/Business/Strategies/SalesTax/SwedenSalesTaxStrategy.cs
@@ -9,15 +9,26 @@ namespace StrategyPattern.Business.Strategies.SalesTax
     {
         public decimal GetTaxFor(Order order)
         {
-            var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
-            var origin = order.ShippingDetails.OriginCountry.ToLowerInvariant();
+            decimal totalTax = 0m;
 
-            if (destination == origin)
+            foreach (var item in order.LineItems)
             {
-                return order.TotalPrice * 0.25m;
+                switch (item.Key.ItemType)
+                {
+                    case ItemType.Food:
+                        totalTax += (item.Key.Price * 0.06m) * item.Value;
+                        break;
+                    case ItemType.Literature:
+                        totalTax += (item.Key.Price * 0.08m) * item.Value;
+                        break;
+                    case ItemType.Service:
+                    case ItemType.Hardware:
+                        totalTax += (item.Key.Price * 0.25m) * item.Value;
+                        break;
+                }
             }
 
-            return 0;
+            return totalTax;
         }
     }
 }
